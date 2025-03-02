e2d633131fcddc62df131f6481d653efe0a2d9c8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-03-02 14:05:20 +0000

    Adding identity block and tags

--- terraform/dev_center.tf
+++ terraform/dev_center.tf
@@ -3,4 +3,10 @@ resource "azurerm_dev_center" "dev_center" {
 
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
 }
