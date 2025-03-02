28e3b1c1fb6d834e670158ef8eb2afdd62af5ff2
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-03-02 14:35:34 +0000

    Adding tags

--- terraform/dev_center_project_environment_type.tf
+++ terraform/dev_center_project_environment_type.tf
@@ -1,24 +1,44 @@
 resource "azurerm_dev_center_environment_type" "sandbox" {
   name          = "sandbox"
   dev_center_id = azurerm_dev_center.dev_center.id
+
+  tags = {
+    Environment = "sandbox"
+  }
 }
 
 resource "azurerm_dev_center_environment_type" "development" {
   name          = "development"
   dev_center_id = azurerm_dev_center.dev_center.id
+
+  tags = {
+    Environment = "dev"
+  }
 }
 
 resource "azurerm_dev_center_environment_type" "integration" {
   name          = "integration"
   dev_center_id = azurerm_dev_center.dev_center.id
+
+  tags = {
+    Environment = "integration"
+  }
 }
 
 resource "azurerm_dev_center_environment_type" "test" {
   name          = "test"
   dev_center_id = azurerm_dev_center.dev_center.id
+
+  tags = {
+    Environment = "test"
+  }
 }
 
 resource "azurerm_dev_center_environment_type" "production" {
   name          = "production"
   dev_center_id = azurerm_dev_center.dev_center.id
+
+  tags = {
+    Environment = "prd"
+  }
 }
