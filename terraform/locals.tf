f37b30fc0cc08fb492edff44141dc5ac3332bb63
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-03-02 14:08:53 +0000

    Update dev centre name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
   resource_group_name = "rg-platform-dev-tools-${var.environment}-${var.location}"
-  dev_center_name     = "dev-molyneux-io-${var.environment}-${var.location}"
+  dev_center_name     = "dev-mx-io-${var.environment}-${var.location}"
 }
